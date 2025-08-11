4c3036c734844f827288c2470bdf5c320ec94217
Xy-0735.github.io <1541016466@qq.com>
2025-08-11 12:29:54 +0800

    Site updated: 2025-08-11 12:29:54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1754886487890";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1754886593933";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
