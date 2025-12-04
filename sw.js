8e28cbf663c54e0c868fe912420194f949c11f41
Xy-0735.github.io <1541016466@qq.com>
2025-12-04 15:23:52 +0800

    Site updated: YYYY-12-2025年12月4日 15:23:49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1759637661970";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1764832963378";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
